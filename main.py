d51dd3964c9f6d4827c7abd82ba30bb1cee1c24b
Danila Bureev <bureevdanil@gmail.com>
2020-11-15 21:51:22 +0300

    4.7
    Clicking all list items at sidebar

--- main.py
+++ main.py
@@ -11,9 +11,24 @@ def driver(request):
     return wd
 
 
+def find_h1(where):
+    where.find_element_by_css_selector("#content h1")
+
+
 def test_example(driver):
+    driver.implicitly_wait(2)
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
-    WebDriverWait(driver, 5).until(EC.title_is("My Store"))
+    apps_ul = driver.find_element_by_id("box-apps-menu")
+    apps_links = []
+    for app in apps_ul.find_elements_by_id("app-"):
+        apps_links.append(app.find_element_by_css_selector("a").get_attribute("href"))
+    for app_link in apps_links:
+        driver.find_element_by_css_selector(f"a[href='{app_link}']").click()
+        find_h1(driver)
+        for i in range(1, len(driver.find_elements_by_css_selector("#app-.selected li"))):
+            driver.find_element_by_css_selector(f"#app-.selected li:nth-child({i+1})").click()
+            find_h1(driver)
+
