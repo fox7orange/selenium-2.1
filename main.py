8a92df5071936bde9395bae2ac45c5b945bb8f27
Danila Bureev <bureevdanil@gmail.com>
2020-11-16 16:30:51 +0300

    5.9.1
    все ли страны и зоны в алфавитном порядке

--- main.py
+++ main.py
@@ -15,10 +15,35 @@ def find_h1(where):
     where.find_element_by_css_selector("#content h1")
 
 
-def test_example(driver):
+def is_alphabetical(driver, col_num: int):
+    rows = driver.find_elements_by_css_selector(".dataTable tr.row")
+    names = [row.find_element_by_xpath(f"./td[{col_num}]").text for row in rows]
+    for i in range(1, len(names)):
+        assert names[i - 1] < names[i]
+
+
+def test_countries(driver):
     driver.implicitly_wait(2)
-    driver.get("http://localhost/litecart/")
-    content = driver.find_element_by_css_selector("div.middle > div.content")
-    for product in content.find_elements_by_css_selector('li.product'):
-        stickers = product.find_elements_by_class_name('sticker')
-        assert len(stickers) == 1
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    is_alphabetical(driver, 5)
+
+
+def test_zones(driver):
+    driver.implicitly_wait(2)
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    rows = driver.find_elements_by_css_selector(".dataTable tr.row")
+    not_zero_zones_links = []
+    for row in rows:
+        zone = row.find_element_by_xpath("./td[6]").text
+        if int(zone) > 0:
+            not_zero_zones_links.append(row.find_element_by_xpath("./td[5]/a").get_attribute("href"))
+    for link in not_zero_zones_links:
+        driver.get(link)
+        is_alphabetical(driver, 3)
+
