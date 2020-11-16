6929ac22f23027331eb4e7f135af47996d478178
Danila Bureev <bureevdanil@gmail.com>
2020-11-16 17:11:06 +0300

    5.9

--- main.py
+++ main.py
@@ -47,3 +47,24 @@ def test_zones(driver):
         driver.get(link)
         is_alphabetical(driver, 3)
 
+
+def test_geo_zones(driver):
+    driver.implicitly_wait(2)
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    rows = driver.find_elements_by_css_selector(".dataTable tr.row")
+    country_links = []
+    for row in rows:
+        country_links.append(row.find_element_by_xpath("./td[3]/a").get_attribute("href"))
+    for link in country_links:
+        driver.get(link)
+        rows = driver.find_elements_by_css_selector(".dataTable tr:not(.header)")
+        names = []
+        for row in rows:
+            tds = row.find_elements_by_css_selector("td")
+            if len(tds) == 4:
+                names.append(tds[2].find_element_by_css_selector("option[selected]").text)
+        for i in range(1, len(names)):
+            assert names[i - 1] < names[i]
