e97df37ffb2ac9a85893c37d2051d9de662483be
Danila Bureev <bureevdanil@gmail.com>
2020-11-16 11:59:19 +0300

    4.8
    Проверка наличия ровно 1 стикера у каждого товара

--- main.py
+++ main.py
@@ -17,18 +17,8 @@ def find_h1(where):
 
 def test_example(driver):
     driver.implicitly_wait(2)
-    driver.get("http://localhost/litecart/admin/")
-    driver.find_element_by_name("username").send_keys("admin")
-    driver.find_element_by_name("password").send_keys("admin")
-    driver.find_element_by_name("login").click()
-    apps_ul = driver.find_element_by_id("box-apps-menu")
-    apps_links = []
-    for app in apps_ul.find_elements_by_id("app-"):
-        apps_links.append(app.find_element_by_css_selector("a").get_attribute("href"))
-    for app_link in apps_links:
-        driver.find_element_by_css_selector(f"a[href='{app_link}']").click()
-        find_h1(driver)
-        for i in range(1, len(driver.find_elements_by_css_selector("#app-.selected li"))):
-            driver.find_element_by_css_selector(f"#app-.selected li:nth-child({i+1})").click()
-            find_h1(driver)
-
+    driver.get("http://localhost/litecart/")
+    content = driver.find_element_by_class_name("content")
+    for product in content.find_elements_by_css_selector('li.product'):
+        stickers = product.find_elements_by_class_name('sticker')
+        assert len(stickers) != 1
