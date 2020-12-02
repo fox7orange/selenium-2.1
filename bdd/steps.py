87491dd5ffb74c1ec28bd62f4b2b0504064f5037
Danila Bureev <bureevdanil@gmail.com>
2020-12-02 15:55:49 +0300

    Update steps.py

--- bdd/steps.py
+++ bdd/steps.py
@@ -1,10 +1,10 @@
-from pytest_bdd import given, when, then
+from pytest_bdd import given, when, then, parsers
 from Apps.app import Application
 
 
-@given('3 products added to the cart')
-def add_products_to_cart(app: Application):
-    app.add_products_to_cart(3)
+@given(parsers.parse('{some:d} products added to the cart'))
+def add_products_to_cart(app: Application, some):
+    app.add_products_to_cart(some)
 
 
 @when('all products are deleted from the cart')
